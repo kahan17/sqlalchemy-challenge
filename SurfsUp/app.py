c090a3103d688cfccc8b011ea5100b42939e7c35
Kahan Shah <kahanshah17@gmail.com>
2024-09-29 00:29:00 -0500

    Final Commit

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,107 @@
-# Import the dependencies.
-
-
+# Import the dependencies
+import numpy as np
+import pandas as pd
+from flask import Flask, jsonify
+from sqlalchemy import create_engine, func
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+import datetime as dt
 
 #################################################
 # Database Setup
 #################################################
 
+# Create engine to connect to the SQLite database
+engine = create_engine("sqlite:///SurfsUp/resources/hawaii.sqlite")
 
-# reflect an existing database into a new model
+# Reflect an existing database into a new model
+Base = automap_base()
 
-# reflect the tables
-
+# Reflect the tables
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return the last 12 months of precipitation data as JSON"""
+    most_recent_date = session.query(func.max(Measurement.date)).scalar()
+    one_year_ago = dt.datetime.strptime(most_recent_date, '%Y-%m-%d') - dt.timedelta(days=365)
+
+    results = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= one_year_ago).all()
+
+    # Convert to dictionary using date as the key and prcp as the value
+    precipitation_data = {date: prcp for date, prcp in results}
+    return jsonify(precipitation_data)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a JSON list of stations from the dataset"""
+    results = session.query(Station.station, Station.name).all()
+    station_list = [{station: name} for station, name in results]
+    return jsonify(station_list)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Return the temperature observations of the most active station for the last year"""
+    most_recent_date = session.query(func.max(Measurement.date)).scalar()
+    one_year_ago = dt.datetime.strptime(most_recent_date, '%Y-%m-%d') - dt.timedelta(days=365)
+
+    # Get the most active station
+    most_active_station = session.query(Measurement.station).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.station).desc()).first()[0]
+
+    # Query the temperature observations for the most active station
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.station == most_active_station).\
+        filter(Measurement.date >= one_year_ago).all()
+
+    temperature_data = [{date: tobs} for date, tobs in results]
+    return jsonify(temperature_data)
+
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def temperature_range(start, end=None):
+    """Return TMIN, TAVG, TMAX for a specified start or start-end range"""
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if end:
+        results = session.query(*sel).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    else:
+        results = session.query(*sel).filter(Measurement.date >= start).all()
+
+    temp_data = list(np.ravel(results))
+    return jsonify(temp_data)
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
